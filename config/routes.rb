e44e3cc8d3cf861cdce9caccc6fb9114b20eb89f
Toni Maki <maki.toni@gmail.com>
2012-03-08 15:15:18 +0200

    Tutorial layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 WwwDesign::Application.routes.draw do
+  root :to => 'pages#home'
+  get "users/new"
+  
+  match '/signup',  :to => 'users#new'
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  get "pages/home"
+  get "pages/about"
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +60,7 @@ WwwDesign::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   
 
   # See how all your routes lay out with "rake routes"
 
