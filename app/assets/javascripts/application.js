6a56016cb53df749d16a4444ff5db2f88eb0afd0
Kiia <kiia.korpi@gmail.com>
2012-04-09 19:29:12 +0300

    Male / female style background (buggy)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -85,7 +85,19 @@ function filterCategories(category_id, $category) {
 	}
 }  
 
-jQuery(function($) {
+$(function() {
+
+	$("#gender_tab li").click(function(){
+		if(!$(this).hasClass("selected")){
+			var id = $(this).attr("id");
+			$("#switch_style").attr("href", "assets/" + id + ".css");
+			$("#gender_tab li").each(function(){
+				$(this).toggleClass('selected');
+			});
+		}
+	});
+	
+	
 	//miehet/naiset changes
 	$("select#category_root").change(function() {
 		// get subtree and replace selects
